b35f8301e55670312e6497680159fa97ba5048e1
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-02-23 12:20:27 +0530

    app create

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const mongoose = require("mongoose");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/bnb";
 
+main.
+    then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 async function main() {
     await mongoose.connect(MONGO_URL);
 };
