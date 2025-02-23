9faf8a1fbe903b0be0a7120c6fe9b15ef4d411ee
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-02-23 12:18:13 +0530

    app create

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/bnb";
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
 app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
