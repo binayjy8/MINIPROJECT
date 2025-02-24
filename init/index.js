5b06fb04e97c078b0a589e3a18cb5c1680661d76
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-02-24 10:06:11 +0530

    Listing Created

--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const data = require("./data.js");
+const initData = require("./data.js");
 const Listing = require("./models/listings.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/bnb";
@@ -15,3 +15,9 @@ main()
 async function main() {
     await mongoose.connect(MONGO_URL);
 };
+
+const initDB = async() => {
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    res.send("data was initialized");
+}
