6dbdbed5c4f1ef612fc61a915852e240fbb0ea49
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-02-26 09:22:37 +0530

    template create

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
     res.send("hi this is root");
