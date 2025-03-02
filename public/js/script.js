b2cb10e59c3560d87da889d388a93a74f4926b4e
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-02 20:27:05 +0530

    adding style

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,8 @@
   
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+      form.addEventListener("submit", event => {
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
