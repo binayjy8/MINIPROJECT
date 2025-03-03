1c8a1797992d91a37bbf6893fcfe872283dae33d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-03 11:38:03 +0530

    schema for server

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/expressError.js");
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/bnb";
 
