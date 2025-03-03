c0619a3cdafa4453d28a37763fef1a0e4842db7e
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-03 11:28:33 +0530

    schema for server

--- app.js
+++ app.js
@@ -104,7 +104,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { statusCode=500, message="Something went wrong"} = err;
-    res.render("error.ejs");
+    res.status(statusCode).render("error.ejs", { message });
     // res.status(statusCode).send(message);
 });
 
