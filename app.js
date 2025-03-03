5428b2343046ee1d9c1cfb675daaf1e857efa517
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-03 11:48:09 +0530

    schema for server

--- app.js
+++ app.js
@@ -34,6 +34,15 @@ app.get("/", (req, res) => {
     res.send("hi this is root");
 });
 
+const validateListing = (req, res, next) => {
+    let { error} = listingSchema.validate(req.body);
+    if (error) {
+        throw new ExpressError(400, resourceLimits.error);
+    } else {
+        next();
+    }
+}
+
 //Index Route
 app.get(
     "/listings",  
