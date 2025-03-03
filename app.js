5ffffba78872221f4aca45e2048642e3c6bba5b8
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-03 10:51:05 +0530

    error handling

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.get("/listings/new", (req, res) => {
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", {listing});
-});
+}));
 
 //Create Route
 app.post(
