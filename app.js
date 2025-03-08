f5ef973c73067e4ca341d807c9bbfede95815385
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-08 21:42:10 +0530

    add review

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ app.all("*", (req, res, next) => {
     next(new ExpressError(404, "This page is not found!"));
 });
 
-app.lo
+app.loc
 
 app.use((err, req, res, next) => {
     let { statusCode=500, message="Something went wrong"} = err;
