3bb8211adce8402c43c7ad2a8b63cfcc367b79b5
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-10 09:35:17 +0530

    button added

--- app.js
+++ app.js
@@ -136,6 +136,8 @@ app.delete(
     wrapAsync(async(req, res) => {
         let { id, reviewId } = req.params;
     }
+
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
 ));
 
 app.all("*", (req, res, next) => {
