f2472315b4b9b648862522d5a0c82585065bafa6
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-10 10:34:23 +0530

    delete route

--- models/listings.js
+++ models/listings.js
@@ -31,7 +31,7 @@ const listingSchema = new Schema ({
 
 listingSchema.post("findOneAndDelete", async (listing) => {
     if (listing) {
-        await Review.deleteMany({})
+        await Review.deleteMany({_id: { $in: listing.reviews } });
     }
 });
 
