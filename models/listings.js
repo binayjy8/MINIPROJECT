c884d4c676509d4e24e5a95ad7ec4952ebc3171f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-10 10:27:17 +0530

    delete route

--- models/listings.js
+++ models/listings.js
@@ -1,6 +1,7 @@
 const { ref } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema ({
     title: {
