d5d927374576fcaf0d18ad6db5564917e4bf6113
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-11 20:53:26 +0530

    delete route

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-    res.send("hi this is root bit root");
+    res.send("hi this is root bit root o");
 });
 
 const validateListing = (req, res, next) => {
