15a7917dcaa42831c3bf056ef27038e4c96ed73a
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-12 11:42:15 +0530

    router added

--- router/listing.js
+++ router/listing.js
@@ -1,2 +1,25 @@
 const express= require("express");
 const router = express.Router();
+
+
+//Index Route
+app.get(
+    "/listings",  
+    wrapAsync(async (req, res) => {
+     const allListings = await Listing.find({});
+     res.render("./listings/index.ejs", {allListings});
+}));
+
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+//Show Route
+app.get(
+    "/listings/:id", 
+    wrapAsync(async (req, res) => {
+     let {id} = req.params;
+     const listing = await Listing.findById(id).populate("reviews");
+     res.render("./listings/show.ejs", {listing});
+}));
