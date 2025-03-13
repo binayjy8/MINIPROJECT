801b5fa35951554f90b922ae1c1c3a74556cdd24
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-13 08:51:50 +0530

    router added

--- router/listing.js
+++ router/listing.js
@@ -1,7 +1,19 @@
 const express= require("express");
 const router = express.Router();
 const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/expressError.js");
 const { listingSchema, reviewSchema } = require("./schema.js");
+
+const validateListing = (req, res, next) => {
+    let { error} = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 
 
 //Index Route
