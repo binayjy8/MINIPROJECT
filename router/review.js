9a9413b34dc9810a18a1b6ff39a4ece97c86a711
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-13 10:21:40 +0530

    review route

--- router/review.js
+++ router/review.js
@@ -5,6 +5,16 @@ const ExpressError = require("../utils/expressError.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 const Review = require("../models/review.js");
 
+const validateReview = (req, res, next) => {
+    let { error} = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
 //Review Route
 router.post(
     "/", 
