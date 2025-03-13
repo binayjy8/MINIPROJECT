d45f9443dc00c680afd67de5b031d9817d1a72cc
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-13 08:50:07 +0530

    router added

--- router/listing.js
+++ router/listing.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const router = express.Router();
 const wrapAsync = require("./utils/wrapAsync.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 
 
 //Index Route
