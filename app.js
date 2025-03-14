66565475fc194751f6fbdc0b62e0492c50b405d8
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-14 10:29:16 +0530

    sessions

--- app.js
+++ app.js
@@ -31,6 +31,10 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret: ""
+}
+
 app.get("/", (req, res) => {
     res.send("hi this is root.");
 });
