c34f3f0c01bcd93dbf9c6f630a6ce4a5764f6f33
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-15 10:14:35 +0530

    user model

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(session(sessionOptions));
 app.use(flash());
 
 app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
