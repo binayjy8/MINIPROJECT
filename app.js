d1508a454f40065e3a74c48ddc1a500a08ed3779
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-15 10:16:03 +0530

    user model

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ExpressError = require("./utils/expressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
-const localStrategy = require("passport-local");
+const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 
 const listings = require("./router/listing.js");
@@ -55,6 +55,7 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
