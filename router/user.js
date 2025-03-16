063f905799343f32da72dd40f7063746bd40dda4
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-16 15:34:46 +0530

    sign route

--- router/user.js
+++ router/user.js
@@ -10,6 +10,6 @@ router.post("/signup", async(req, res) => {
     let { email, username, password } = req.body;
     const newUser = new User(username, password);
     const registerdUser = await User.register(newUser, password);
-})
+});
 
 module.exports = router;
