0987094a99cf0ace2cd83994551dce96b40d62ae
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-16 18:58:48 +0530

    sign route

--- router/user.js
+++ router/user.js
@@ -2,6 +2,7 @@ const express= require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
@@ -25,5 +26,15 @@ router.post(
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
+
+router.post(
+    "/login", 
+        passport.authenticate("local", { 
+            failureRedirect: "/login", 
+            failureFlash: true,
+        }),
+        async(req, res) => {
+            res.send("Welcome! You are logged in");
+        });
 
 module.exports = router;
