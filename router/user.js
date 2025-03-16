1f06ca40104a75a50ea388735c3201a86cfe7ce2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-16 15:54:20 +0530

    sign route

--- router/user.js
+++ router/user.js
@@ -1,17 +1,25 @@
 const express= require("express");
 const router = express.Router();
 const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", async(req, res) => {
-    let { email, username, password } = req.body;
+router.post(
+    "/signup", 
+    wrapAsync(async(req, res) => {
+        try {
+            let { email, username, password } = req.body;
     const newUser = new User({ email, username });
     const registerdUser = await User.register(newUser, password);
     console.log(registerdUser);
     res.redirect("/listings");
-});
+        } catch (e) {
+
+        }
+    
+}));
 
 module.exports = router;
