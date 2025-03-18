9f7cce4f1cd1d668880c4d6a5294b55448c1886f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-18 19:16:26 +0530

    sign route

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
+        req
         req.flash("error", "you must be logged in");
         return res.redirect("/login");
     }
