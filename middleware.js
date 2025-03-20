11e66bd4e0fff9deb00ae21f94b5a4847b4966a2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-20 21:47:55 +0530

    sign route

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-module.exports.reSave = ("logout of the device is alwy");
+module.exports.reSave = ("logout of the device is alwys");
