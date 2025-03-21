9c32617ce2bf14d735ed0358336296cc429ccbb2
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-21 21:50:34 +0530

    sign route

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-module.exports.reSave = ("logo off the chinos gifted he");
+module.exports.reSave = ("logo off the chinos gifted hel");
