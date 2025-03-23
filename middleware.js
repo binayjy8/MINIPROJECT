9adf006ffbaf1727fc1c8bef07711315d09aa6b0
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-23 12:44:22 +0530

    sign route

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-module.exports.reSave = ("Hello with the global inter");
+module.exports.reSave = ("Hello with the global intern");
