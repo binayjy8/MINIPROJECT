1c548d2e916877a20023bf62105249fb5bf656fe
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-03-28 08:43:14 +0530

    sign route

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Hello world give the quick lession pl");
+console.log("Hello world give the quick lession ple");
