3a7eda8d44ad907828244016b8017366f0892e31
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-01 21:24:15 +0530

    coding

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Count your struggling days and determine your s");
+console.log("Count your struggling days and determine your sl");
