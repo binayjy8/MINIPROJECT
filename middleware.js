2d9868e3fcf1c52162061b2058a496f78a78fe30
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-04 11:46:32 +0530

    function

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Developing the next big things we are");
+console.log("Developing the next big things we are t");
