f4a11eb875e6b86fe095dab656341073bd738071
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-09 09:54:46 +0530

    adding coding

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Readding the talent to the bench mar");
+console.log("Readding the talent to the bench mark");
