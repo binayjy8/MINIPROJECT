1f3eda791169966c20514bca82230f49cb8f2d6b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-10 12:32:06 +0530

    pushing

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Sleding is the way of git command an");
+console.log("Sleding is the way of git command and");
