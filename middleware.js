75f9cad489d81593327c2e4e799d087b49766448
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-12 20:53:01 +0530

    allow

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("All to the amount the of the given all with the hello world and mov");
+console.log("All to the amount the of the given all with the hello world and movi");
