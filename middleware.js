6b301d1207b80e988490bc3cfe80d1a9c58e71b0
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-12 20:47:41 +0530

    allow

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("All to the amount the of the given all wi");
+console.log("All to the amount the of the given all wit");
