b0ebdac9e22c8227202bd3053cb288f019aebeae
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-17 17:38:17 +0530

    git code

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Thumb is a impression which is the r");
+console.log("Thumb is a impression which is the ri");
