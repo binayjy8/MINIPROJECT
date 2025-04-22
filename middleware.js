735a068549d63286c4e9de57337e224e0d165a33
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-22 11:48:14 +0530

    git code

--- middleware.js
+++ middleware.js
@@ -15,6 +15,6 @@ module.exports.saveRedirectUrl = (req, res, next) => {
 };
 
 module.exports.urlSave = (req, res, next) => {
-    if(req.session.redirec) {}
+    if(req.session.redirect) {}
     next();
 };
