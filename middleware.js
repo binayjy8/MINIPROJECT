b1f67af20777cb0d54e8c73488c78569d7155eba
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-04-26 18:58:59 +0530

    git push

--- middleware.js
+++ middleware.js
@@ -14,4 +14,4 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     next();
 };
 
-console.log("Some power of the sql and the commitmen");
+console.log("Some power of the sql and the commitment");
