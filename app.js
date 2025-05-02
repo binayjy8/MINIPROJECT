7a51114e4a3ef8ecdc9a9544826ca60d59b2d791
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-05-02 11:23:22 +0530

    print

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("hello world global warming with the effectl");
+console.log("hello world global warming with the effectly");
