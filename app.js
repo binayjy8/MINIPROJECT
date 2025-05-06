f1c04561dd6fef296818996b6c12f4977dc3fcc5
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-05-06 18:52:20 +0530

    print

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("Sound like mind and sound soud l");
+console.log("Sound like mind and sound soud lk");
