d0ce8b4adb82d8c5faa04896faa4eb0aa30f89d3
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-05-16 11:24:37 +0530

    code

--- app.js
+++ app.js
@@ -83,5 +83,3 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
-
-console.log("Giving this adding fragmentation wonderala waterpark with other ap");
