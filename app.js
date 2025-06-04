8af1a89dbec9f42b897cd4a66adff6cd5378080b
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-06-04 20:11:19 +0530

    code

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("The microsoft compaany is a comment as");
+console.log("The microsoft compaany is a comment as c");
