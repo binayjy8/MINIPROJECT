c699f478c076d3b3d1c474f7703fdf22ab6c4752
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-06-08 12:09:51 +0530

    code

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("Hello This is a five star rating w");
+console.log("Hello This is a five star rating we");
