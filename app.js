9fd3548e82008a05b8995e79fcfefbdfa452298f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-06-21 15:12:30 +0530

    code

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("Is there a cramp in hand of the batter is the har");
+console.log("Is there a cramp in hand of the batter is the hard");
