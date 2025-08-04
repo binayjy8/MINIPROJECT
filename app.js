07f0ad788a629acbe7e43665ef1f18d45d6bb031
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-08-04 12:32:20 +0530

    code

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("To appartmental rental applicati");
+console.log("To appartmental rental applicatio");
