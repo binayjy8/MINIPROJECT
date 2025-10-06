699e6402a53cdd92dcb1981b16a4eaf7e37ea426
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2025-10-06 20:29:38 +0530

    code push

--- app.js
+++ app.js
@@ -84,4 +84,4 @@ app.listen(8080, () => {
     console.log("Listening to the port : 8080");
 });
 
-console.log("Killiya password is the power of ");
+console.log("Killiya password is the power o ");
