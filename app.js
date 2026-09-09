8ffcfb6af5092f06050ba1c2b10d73a5a36df12f
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2026-09-09 21:29:12 +0530

    airbnb

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const sessionOptions = {
 };
 
 app.get("/", (req, res) => {
-    res.send(" hello is root and are test.");
+    res.send(" hello is root and are testi.");
 });
 
 app.use(session(sessionOptions));
